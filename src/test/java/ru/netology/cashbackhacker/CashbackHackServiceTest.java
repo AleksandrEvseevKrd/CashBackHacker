ce3ccd16e8e17d33a9c863cbd4a454fde986d3d8
Aleksander Evseev <snooper777@mail.ru>
2023-08-29 21:12:32 +0300

    first commit

--- src/test/java/ru/netology/cashbackhacker/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/cashbackhacker/CashbackHackServiceTest.java
@@ -1,39 +1,40 @@
 package ru.netology.cashbackhacker;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
-    public void cashbackHackServiceTestFirst() {
+    @org.junit.Test
+    public void CashbackHackServiceTestFirst() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(0), 1_000);
+        org.junit.Assert.assertEquals(1_000, cashbackHackService.remain(0));
     }
 
-    @org.testng.annotations.Test
-    public void cashbackHackServiceTestSecond() {
+    @org.junit.Test
+    public void CashbackHackServiceTestSecond() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(1), 999);
+        org.junit.Assert.assertEquals(999, cashbackHackService.remain(1));
     }
 
-    @org.testng.annotations.Test
-    public void cashbackHackServiceTestThird() {
+    @org.junit.Test
+    public void CashbackHackServiceTestThird() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(476), 524);
+        org.junit.Assert.assertEquals(524, cashbackHackService.remain(476));
     }
 
-    @org.testng.annotations.Test
-    public void cashbackHackServiceTestFourth() {
+    @org.junit.Test
+    public void CashbackHackServiceTestFourth() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(999), 1);
+        org.junit.Assert.assertEquals(1, cashbackHackService.remain(999));
     }
 
-    @org.testng.annotations.Test
-    public void cashbackHackServiceTestFifth() {
+    @org.junit.Test
+    /// Баг
+    public void CashbackHackServiceTestFifth() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(1_000), 0);
+        org.junit.Assert.assertEquals(0, cashbackHackService.remain(1_000));
     }
 
-    @org.testng.annotations.Test
-    public void cashbackHackServiceTestSixth() {
+    @org.junit.Test
+    public void CashbackHackServiceTestSixth() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(1_001), 999);
+        org.junit.Assert.assertEquals(999, cashbackHackService.remain(1_001));
     }
 }
